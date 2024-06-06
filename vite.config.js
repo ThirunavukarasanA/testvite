17dffb01a3691305a99b17b0f20b40be3ef9c0d1
ThirunavukarasanA <thirunavukarasan@rvsgroup.com>
2024-06-06 09:55:00 +0530

    base

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
-// https://vitejs.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
+  base: "/",
   plugins: [react()],
-})
+});
